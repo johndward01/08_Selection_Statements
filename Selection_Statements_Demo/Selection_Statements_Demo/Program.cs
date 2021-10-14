b0dbccae0936c6ff225ba721cf387c8201616335
johndward01 <johndward01@gmail.com>
2021-10-14 15:39:59 -0500

    update

--- Selection_Statements_Demo/Selection_Statements_Demo/Program.cs
+++ Selection_Statements_Demo/Selection_Statements_Demo/Program.cs
@@ -7,19 +7,26 @@ namespace Selection_Statements_Demo
         static void Main(string[] args)
         {
             //The single if statement
-            bool condition1 = true;
+            //bool condition1 = true;
 
-            if (condition1)
-            {
-                Console.WriteLine("The variable is set to true.");
-                Console.WriteLine();
-            }
-            Console.WriteLine("If there is nothing above this sentence then the condition was false and that code didn't run.");
+            //if (!condition1)
+            //{
+            //    Console.WriteLine("The variable is set to true.");
+            //    Console.WriteLine();
 
-            ////The if/else statement
-            //int condition2 = 1;
+            //}
+            //else
+            //{
+            //     Console.WriteLine("Another statement");                
+            //}
 
-            //if (condition2 == 1)
+
+            //Console.WriteLine("If there is nothing above this sentence then the condition was false and that code didn't run.");
+
+            //The if/else statement
+
+
+            //if (condition2 != 1)
             //{
             //    Console.WriteLine("This variable is equal to 1.");
             //}
@@ -28,19 +35,145 @@ namespace Selection_Statements_Demo
             //    Console.WriteLine("This variable is not equal to 1.");
             //}
 
-            ////The if/else-if/else statement
+            //The if/else if/else statement
+            //int condition2 = 1;
+
+            //switch (condition2)
+            //{
+            //    case 0:
+            //        Console.WriteLine("The variable is 0.");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("The variable is 1.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("The variable is 2.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("The variable is 3.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Tired of writing more cases.");
+            //        break;
+            //}
 
             //if (condition2 < 0)
             //{
-            //    Console.WriteLine("The variable is set negative.");
+            //    Console.WriteLine("The variable is negative.");
             //}
             //else if (condition2 > 0)
             //{
             //    Console.WriteLine("The variable is positive.");
             //}
+            //else if (!(condition2 == 0))
+            //{
+            //    Console.WriteLine("Some nonsense");
+            //}
             //else
             //{
             //    Console.WriteLine("The variable is 0.");
+            //}
+
+
+            //bool condition1 = true;
+
+            //switch (condition1)
+            //{
+            //    case true:
+            //        Console.WriteLine("True");
+            //        break;
+            //    case false:
+            //        Console.WriteLine("False");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Neither true or false (this will never run!)");
+            //        break;
+            //}
+
+            //int votingAge;
+            //bool isCitizen;
+            //string record;
+
+            //Console.WriteLine("How old are you?");
+            //votingAge = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Are you a legal US citizen?");
+            //Console.Write("Type true or false: ");
+            //isCitizen = bool.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Do you have a clean record?");
+            //Console.Write("Type Y / N : ");
+            //record = Console.ReadLine().ToLower();
+
+            //if (votingAge >= 18 && isCitizen && record == "y")
+            //{
+            //    Console.WriteLine("Can vote in National & State Election");
+            //}
+            //else if ( record == "y" || votingAge >= 18 && isCitizen)
+            //{
+            //    Console.WriteLine("You can vote National Election but not the state"); // fix logic 
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry! You are not old enough to vote!");
+            //}
+
+            //Console.WriteLine("What day of the week is it?");
+            //Console.WriteLine("Please enter 0 for Sunday, 1 for Monday, 2 for Tuesday, etc...");
+            //int day = int.Parse(Console.ReadLine());
+
+            //string something = "";
+
+
+            //switch (something)
+            //{
+            //    case "dkfjdkfjkd":
+            //        Console.WriteLine("if");
+            //        break;
+            //    case temp:
+            //        Console.WriteLine("else if");
+            //    default:
+            //        Console.WriteLine("else");
+            //        break;
+            //}
+
+
+
+            //Console.WriteLine("Enter a day of the week");
+            //var day = Console.ReadLine().ToLower();
+
+            //switch (day)
+            //{
+            //    case "sunday":
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    case "monday":
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case "tuesday":
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case "wednesday":
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case "thursday":
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case "friday":
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case "saturday":
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case "":
+            //        Console.WriteLine("An empty string");
+            //        break;
+            //    case " ":
+            //        Console.WriteLine("An empty space");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Not a day of the week");
+            //        break;
             //}
 
 
@@ -134,6 +267,30 @@ namespace Selection_Statements_Demo
             //        Console.WriteLine();
             //    }
             //}
+
+
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
--- Selection_Statements_Exercise/Selection_Statements_Exercise/Program.cs
+++ Selection_Statements_Exercise/Selection_Statements_Exercise/Program.cs
@@ -7,37 +7,44 @@ namespace Selection_Statements_Exercise
         static void Main(string[] args)
         {
             //Part 1: (if, else-if)
+            int myInt = 10;
 
-            var r = new Random();
+            if (myInt > 10 && "Hey" == "Hey") { Console.WriteLine("This code"); }
 
-            int favoriteNumber = r.Next(1, 1000);
+            //Random r = new Random();
 
-            Console.Write("Try to guess my favorite number: ");
-            var guess = int.Parse(Console.ReadLine());
+            //int favoriteNumber = 100;
 
-            if (guess < favoriteNumber)
-            {
-                Console.WriteLine("too low");
-            }
-            else if (guess > favoriteNumber)
-            {
-                Console.WriteLine("too high");
-            }
-            else
-            {
-                Console.WriteLine("You got it!");
-            }
+            //Console.WriteLine("Welcome to the number guessing game!");
+            //Console.WriteLine();
+            //Console.Write("Please enter a number between 1 - 1000: ");
+            //var guess = Convert.ToInt32(Console.ReadLine());
 
-            
+            //if (guess < favoriteNumber)
+            //{
+            //    Console.WriteLine("too low");
+            //}
+            //else if (guess > favoriteNumber)
+            //{
+            //    Console.WriteLine("too high");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You got it!");
+            //}
+
+
 
             //Part 2: (Switch/Case)
+            Console.WriteLine("Please choose from the following subjects: PE, History, Math, Reading, and Science.");
 
-            Console.WriteLine("Enter your favorite subject in school: ");
+            Console.Write("Enter your favorite subject in school: ");
             var subject = Console.ReadLine();
-            
+
             switch (subject.ToLower())
             {
                 case "pe":
+                case "physical education":
                     Console.WriteLine("Your favorite subject is pe.");
                     break;
                 case "history":
@@ -53,11 +60,10 @@ namespace Selection_Statements_Exercise
                     Console.WriteLine("Your favorite subject is science.");
                     break;
                 default:
-                    Console.WriteLine($"This isn't a subject on the list. " +
-                                      $"I'm sure that doesn't make it less important...");
+                    Console.WriteLine($"This isn't a subject on the list. (Check your spelling and try again) ");
                     break;
             }
-            
+
 
         }
     }
